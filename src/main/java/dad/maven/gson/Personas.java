3c13d51807d0f39f4cf3e14f3ea31263f358b7f1
joelhf3 <joel_hf3@hotmail.com>
2020-10-02 18:36:01 +0100

    primera version

--- src/main/java/dad/maven/gson/Personas.java
+++ src/main/java/dad/maven/gson/Personas.java
@@ -2,28 +2,71 @@ package dad.maven.gson;
 
 import java.util.Scanner;
 
+import com.google.gson.GsonBuilder;
+
 public class Personas {
 
+	String name;
+	String surname;
+	int age;
+	
 	public Personas() {
 	}
 
 	public Personas(String name, String surname, int age) {
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
 	}
 
 	public void RecogerDatos() {
 		
 		Scanner scn = new Scanner(System.in);
 		
-		System.out.println("Introduzca nombre:");
-		String name = scn.nextLine();
-		
-		System.out.println("Introduzca apellido:");
-		String surname = scn.nextLine();
-		
-		System.out.println("Introduzca edad:");
-		int age = Integer.parseInt(scn.nextLine());
-		
-		Personas person = new Personas(name, surname, age);
-	}
-	
+		try
+		{				
+			do
+			{
+				System.out.println("Introduzca nombre:");
+				name = scn.nextLine();
+				
+			}while(name.equals(""));		
+				
+			do
+			{
+				System.out.println("Introduzca apellido:");
+				surname = scn.nextLine();
+				
+			}while(surname.equals(""));
+			
+			do
+			{
+				try
+				{
+					System.out.println("Introduzca edad:");
+					age = Integer.parseInt(scn.nextLine());
+					break;
+				}
+				catch(Exception e)
+				{
+					continue;
+				}
+				
+			}while(true);
+			
+			Personas person = new Personas(name, surname, age);
+			
+			String json = new GsonBuilder().setPrettyPrinting().create().toJson(person);
+			System.out.println(json);
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error " + e.getMessage().toString());
+		}
+		finally
+		{
+			scn.close();
+		}		
+	}	
 }
